2ea161e1e09f68ff7912115d4a012d367bb56ce7
AbhinabaSaha-git <avinabosaha11@gmail.com>
2022-06-25 17:57:47 +0530

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print('this is inside ML model')
+
+def model1():
+    print('this is in model 1')
+
+def model2():
+    print('this is in model 2')
+
+def model3():
+    print('this is in model 3')
